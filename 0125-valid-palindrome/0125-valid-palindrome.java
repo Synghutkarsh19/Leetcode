ba4cc5a73b5ee72f3d70d764d70f95d6119b0cd1
Utkarsh Singh <singhutkarsh1900@gmail.com>
2023-10-10 20:49:18 +0530

    Time: 29 ms (16.66%), Space: 44.8 MB (13.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,8 +4,6 @@ class Solution {
         s = s.replaceAll(" + "," ");
         s = s.replaceAll(" ","");
         s = s.toLowerCase();
-        System.out.println(s);
-        System.out.println(s.length());
         for(int i = 0; i<s.length()/2; i++){
             if(s.charAt(i) != s.charAt(s.length()-1-i)) return false;
         }
