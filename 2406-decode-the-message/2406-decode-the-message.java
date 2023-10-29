4581e89f09b573294b410e7694d35a96943f4fda
Utkarsh Singh <singhutkarsh1900@gmail.com>
2023-10-30 00:25:11 +0530

    Time: 16 ms (14.92%), Space: 43.9 MB (31.45%) - LeetHub

---
+++ 2406-decode-the-message/2406-decode-the-message.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        key = key.replaceAll(" ", "");
+        System.out.println(key.length());
+        ArrayList<Character> arr = new ArrayList<>();
+        for(int i = 0; i<key.length(); i++){
+            if(!arr.contains(key.charAt(i)))arr.add(key.charAt(i));
+        }
+
+        String res = "";
+        for(int i = 0;i < message.length(); i++){
+            if(message.charAt(i) == ' '){
+                res+= " ";
+                continue;
+            }
+            int ascii = arr.indexOf(message.charAt(i))+97;
+            char ch = (char) ascii;
+            res+=String.valueOf(ch);
+
+        }
+
+        return res;
+    }
+}
